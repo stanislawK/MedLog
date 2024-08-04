573791e8947ea8406447817ed73462ef0f86c651
stanislawK <stanislaw.krupienko@gmail.com>
2024-08-05 00:13:36 +0200

    6 backend gunicorn setup

--- Makefile
+++ Makefile
@@ -12,3 +12,5 @@ bash:
 	$(COMPOSE_DEV) exec medlog bash
 psql:
 	$(COMPOSE_DEV) exec postgres psql -U postgres
+watch-fe:
+	npx tailwindcss -i ./medlog/static/src/input.css -o ./medlog/static/src/output.css --watch
--- run-backend.sh
+++ run-backend.sh
@@ -10,5 +10,5 @@ if [ $DEBUG == True ]; then
     python manage.py runserver 0.0.0.0:8000
 else
     python manage.py collectstatic --noinput &&
-    gunicorn -w 2 core.wsgi:application --bind 0.0.0.0:8000
+    gunicorn -w 2 medlog.wsgi:application --bind 0.0.0.0:8000
 fi
