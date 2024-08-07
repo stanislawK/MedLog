b444a3733139567c33302b69baf7e6a64947443a
stanislawK <stanislaw.krupienko@gmail.com>
2024-08-07 23:27:40 +0200

    10 - language toggle

--- medlog/medlog/urls.py
+++ medlog/medlog/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("", include("core.urls")),
+]
