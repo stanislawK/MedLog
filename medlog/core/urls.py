d06bcdfac4761b10df5667472b6031f78538a621
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2024-12-11 21:34:52 +0100

    51 - working navbar (#52)

--- medlog/core/urls.py
+++ medlog/core/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path("dashboard/add-log/<str:req_date>/", views.add_log_form, name="add-log"),
     path("dashboard/add-log/", views.add_log_form, name="add-log"),
     path("dashboard/add-medicine/", views.add_medicine_view, name="new_medicine"),
+    path("dashboard/visits/", views.visits_view, name="visits"),
+    path("dashboard/logs-history/", views.logs_history_view, name="logs_history"),
+    path("dashboard/logs-stats/", views.logs_stats_view, name="logs_stats"),
 ]
