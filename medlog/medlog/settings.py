8397556192013ab6ed34386132a6cf392ebd614c
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2025-03-22 22:04:04 +0100

    83 - nginx fixes for serving staticfiles (#84)

--- medlog/medlog/settings.py
+++ medlog/medlog/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = [os.getenv("TRAEFIK_HOST")]
+ALLOWED_HOSTS = [os.getenv("TRAEFIK_HOST"), os.getenv("TRAEFIK_HOST_PROD")]
 
 
 # Application definition
