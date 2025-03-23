94081493d790ebca082839564186ba14a90645df
stanislawK <stanislaw.krupienko@gmail.com>
2025-03-23 23:06:45 +0100

    87 - logging settings

--- medlog/medlog/settings.py
+++ medlog/medlog/settings.py
@@ -159,3 +159,17 @@ STATICFILES_FINDERS = (
 
 LOGIN_URL = "/?loginModal=true"
 AUTH_USER_MODEL = "core.User"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
